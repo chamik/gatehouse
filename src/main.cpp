28e24eea1a2b4d76a7a269973f37d59b947cb21d
mariansam <margotka000@gmail.com>
2019-11-08 15:58:05 +0100

    add some stuff, that will be rewritten anyway...

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,17 @@
 #define SS_PIN 10
 #define RST_PIN 9
 
-typedef char user_t[8];
+typedef uint8_t user_t[4];
+
+enum CardType {
+    Unauthorized,
+    Boi,
+    Master
+};
 
 user_t master;
-user_t bois[8];
+user_t *bois;
+int n_users = 0;
 
 MFRC522 mfrc(SS_PIN, RST_PIN);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -31,10 +38,36 @@ void loadUsers() {
     for (int i = 0; i < 8; i++) {
         master[i] = EEPROM.read(i + 1);
     }
-    
+
     for (int i = 0; i < count; i++) {
         loadUser(i);
     }
+}
+
+CardType checkCard() {
+    if (mfrc.uid.uidByte[0] == master[0] && 
+        mfrc.uid.uidByte[1] == master[1] &&
+        mfrc.uid.uidByte[2] == master[2] &&
+        mfrc.uid.uidByte[3] == master[3])
+        return Master;
+
+    for (int i = 0; i < n_users; i++)
+    {
+        if (mfrc.uid.uidByte[0] == bois[i][0] &&
+            mfrc.uid.uidByte[1] == bois[i][1] &&
+            mfrc.uid.uidByte[2] == bois[i][2] &&
+            mfrc.uid.uidByte[3] == bois[i][3])
+            return Boi;
+    }
+    
+    return Unauthorized;
+}
+
+void masterMode() {
+    Serial.println("boi we got master");
+    lcd.print("MASTER");
+
+    // load new cards and do some other boring stuff
 }
 
 void setup() {
@@ -47,6 +80,10 @@ void setup() {
 
     SPI.begin();
     mfrc.PCD_Init();
+    master[0] = 0xA0;
+    master[1] = 0xDD;
+    master[2] = 0x49;
+    master[3] = 0x1A;
 
     lcd.init();
     lcd.backlight();
@@ -57,5 +94,20 @@ void setup() {
 
 
 void loop() {
-    
+    if (!mfrc.PICC_IsNewCardPresent())
+        return;
+
+    if (!mfrc.PICC_ReadCardSerial())
+        return;
+
+    CardType cardType = checkCard();
+    if (cardType == Master) {
+        masterMode();
+    } else if (cardType == Boi) {
+        Serial.println("boi we got some boi");
+        lcd.print("Welcome you motherfucker");
+    } else {
+        Serial.println("boi we got some špión here");
+        lcd.print("go out you špión");
+    }
 }
