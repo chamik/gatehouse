36dd74d4b2f8a3f35cd53d6c7cdc41765a7d0e81
Jakub Hampl <jakub.hampl@email.cz>
2019-11-29 19:41:03 +0100

    add comments and fix lcd

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #define SS_PIN 10
 #define RST_PIN 9
 #define BUTTON_PIN A0
-#define WELCOME_MESSAGE " I AM READY UWU"
+#define WELCOME_MESSAGE " OH HELLO THERE"
 
 typedef uint8_t user_t[4];
 
@@ -25,15 +25,18 @@ user_t master;
 user_t *bois;
 int n_users = 0;
 
+// Init RFID reader and lcd
 MFRC522 mfrc(SS_PIN, RST_PIN);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// Load user from specified place in EEPROM
 void loadUser(char index) {
     for (int i = 0; i < 4; i++) {
         bois[index][i] = EEPROM.read(5 + (4 * index) + i);
     }
 }
 
+// Load all users from EEPROM
 void loadUsers() {
     n_users = EEPROM.read(0);
 
@@ -48,6 +51,8 @@ void loadUsers() {
     }
 }
 
+
+// Check the type of the card
 CardType checkCard() {
     if (mfrc.uid.uidByte[0] == master[0] && 
         mfrc.uid.uidByte[1] == master[1] &&
@@ -67,6 +72,7 @@ CardType checkCard() {
     return Unauthorized;
 }
 
+// Senpai uwu
 void masterMode() {
     Serial.println("I wasn't expecting you so early senpai *blush*"); // I want to fucking die
     lcd.clear();
@@ -74,13 +80,18 @@ void masterMode() {
 
     // load new cards and do some other boring stuff
     while (true) {
+        // Is new card present?
         if (!mfrc.PICC_IsNewCardPresent())
             continue;
 
+        // Is there a card?
         if (!mfrc.PICC_ReadCardSerial())
             continue;
 
+        // Get info about the card
         CardType cardType = checkCard();
+
+        // Card is known, delete it from the EEPROM
         if (cardType == Boi) {
             for (int i = 0; i < n_users; i++)
             {
@@ -106,13 +117,12 @@ void masterMode() {
             n_users -= 1;
             EEPROM.write(0, n_users);
 
+            lcd.clear();
             Serial.print("I'll get rid of him... *loads gun with religious intent*");
-            lcd.print(" THIS IS SO SAD");
-            lcd.setCursor(0, 1);
-            lcd.print(" PLAY DESPACITO");
-            lcd.home();
+            lcd.print("  BEGONE THOT!");
             delay(500);
 
+        // Card is not known, save it into the EEPROM!
         } else if (cardType == Unauthorized) {
             bois = (user_t*) realloc(bois, (n_users + 1) * sizeof(user_t));
 
@@ -131,25 +141,31 @@ void masterMode() {
             EEPROM.write(0, n_users);
             
             Serial.println("new boi added xd");
+            lcd.clear();
             lcd.print("NEW BOI ADDED XD");
             delay(500);
 
+        // The card is master, leave master mode
         } else if (cardType == Master) {
             Serial.println("so long master uwu...");
+            lcd.clear();
             lcd.print(" BYE SENPAI UWU");
             delay(1000);
             return;
         }
 
+        lcd.clear();
         lcd.print("EPIC MASTER MODE");
     }
 }
 
 void setup() {
+    // Init all the things
     pinMode(OPEN_BUTTON, OUTPUT);
     pinMode(WIPE_BUTTON_ALWAYS_ON, OUTPUT);
     pinMode(RELAY, OUTPUT);
     pinMode(BUTTON_PIN, INPUT);
+
     digitalWrite(WIPE_BUTTON_ALWAYS_ON, HIGH);
     digitalWrite(OUTPUT, HIGH);
     digitalWrite(RELAY, HIGH);
@@ -175,40 +191,53 @@ void setup() {
 
 
 void loop() {
+    // Check if open button is pressed, if yes opend door
     Serial.println(analogRead(BUTTON_PIN));
     if (analogRead(BUTTON_PIN) < 5) {
-        digitalWrite(RELAY, LOW);
-        delay(3000);
-        digitalWrite(RELAY, HIGH);
-
+        openDoor();
         return;
     }
 
+    // Is new card present?
     if (!mfrc.PICC_IsNewCardPresent()) 
         return;
 
+    // Is there a card?
     if (!mfrc.PICC_ReadCardSerial())
         return;
 
-    
+    // Get info about the card
     CardType cardType = checkCard();
+
+    // If master, enter master mode
     if (cardType == Master) {
         masterMode();
+
+    // If known, open door
     } else if (cardType == Boi) {
         Serial.println("i know the taste of this one uwu");
+        lcd.clear();
         lcd.print(" WELCOME HUMAN!");
+        openDoor();
 
-        digitalWrite(RELAY, LOW);
-        delay(3000);
-        digitalWrite(RELAY, HIGH);
+    // Not known, begone thot!
     } else {
         Serial.println("Impossible. Perhaps the archives are incomplete?");
+        lcd.clear();
         lcd.print("     OPENED");
         lcd.setCursor(0, 1);
-        lcd.print("(jk go away lol)");
+        lcd.print(" jk go away lol");
         lcd.home();
         delay(1000);
     }
 
+    lcd.clear();
     lcd.print(WELCOME_MESSAGE);
+}
+
+// Open Sesame!
+void openDoor() {
+    digitalWrite(RELAY, LOW);
+    delay(3000);
+    digitalWrite(RELAY, HIGH);
 }
